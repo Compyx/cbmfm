c083d054d7476839fb8ec88bacec24a22241e749
compyx <b.wassink@ziggo.nl>
2018-04-23 20:52:48 +0200

    Update the GtkApplication main driver
    Set up and clean up logging, add signal handler for the 'open' signal, allowing opening files from the command line.

--- src/gui/main.c
+++ src/gui/main.c
@@ -3,6 +3,9 @@
 /** \file   src/gui/main.c
  * \brief   Application driver
  *
+ * Main application driver, sets up the application, handles opening any files
+ * passed on the command line, and sets up logging.
+ *
  * \author  Bas Wassink <b.wassink@ziggo.nl>
  */
 
@@ -57,6 +60,43 @@ static void on_shutdown(GApplication *app, gpointer data)
 }
 
 
+/** \brief  Handler for the 'open' event of the GtkApplication
+ *
+ * When the application is started, all command line arguments are assumed to
+ * be files to open (due to the G_APPLICATION_HANDLES_OPEN flag). This function
+ * is supposed to open these files using auto-detect (not implemented yet).
+ *
+ * \param[in]   app     application
+ * \param[in]   files   array of files to open
+ * \param[in]   n_files number of files in array
+ * \param[in]   hint    hint for the \a app (unused)
+ * \param[in]   data    extra event data (unused)
+ */
+static void on_open(
+        GApplication *app,
+        gpointer files,
+        gint n_files,
+        gchar *hint,
+        gpointer data)
+{
+    int i;
+    GFile **gfiles = (GFile **)files;
+
+    cbmfm_log_info("got %d files to open:\n", n_files);
+    for (i = 0; i < n_files; i++) {
+        GFile *gf = gfiles[i];
+
+        if (gf != NULL) {
+            char *path = g_file_get_path(gf);
+            if (path != NULL) {
+                cbmfm_log_info("file %d: '%s'\n", i, path);
+                g_free(path);
+            }
+        }
+    }
+}
+
+
 /** \brief  Application driver
  *
  * \param[in]   argc    argument count
@@ -75,9 +115,10 @@ int main(int argc, char **argv)
     cbmfm_log_set_level(CBMFM_LOG_DEBUG);
     cbmfm_log_set_file("gui.log");
 
-    app = gtk_application_new("nl.compyx.cbmfm", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new("nl.compyx.cbmfm", G_APPLICATION_HANDLES_OPEN);
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
     g_signal_connect(app, "shutdown", G_CALLBACK(on_shutdown), NULL);
+    g_signal_connect(app, "open", G_CALLBACK(on_open), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
